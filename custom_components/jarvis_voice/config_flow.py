acf945c71347f7564b1e9245221dc2cebf05205d
DenysKovtunUA <denys.kovtun.ua@gmail.com>
2026-08-16 21:36:31 +0300

    Remove overridden init from options flow to match modern HA api

--- custom_components/jarvis_voice/config_flow.py
+++ custom_components/jarvis_voice/config_flow.py
@@ -47,24 +47,22 @@ class JarvisVoiceConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 class JarvisVoiceOptionsFlowHandler(config_entries.OptionsFlow):
     """Управление изменением таймаута через кнопку 'Параметры'."""
 
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Инициализация через вызов родительского класса HA."""
-        # Передаем запись в родительский класс, HA сам все запишет куда надо
-        super().__init__(config_entry)
+    # ВЫЧИЩЕНО: Полностью удаляем __init__. 
+    # Home Assistant сам инициализирует объект и нативно пропишет self.config_entry!
 
     async def async_step_init(self, user_input=None) -> FlowResult:
         """Вызывается при клике на 'Параметры' (Configure) на плитке интеграции."""
         if user_input is not None:
             _LOGGER.info("Updating Jarvis Voice Engine timeout parameters to: %ss", user_input[CONF_TIMEOUT])
             
-            # Используем встроенное свойство self.config_entry
+            # Используем нативное свойство self.config_entry
             self.hass.config_entries.async_update_entry(
                 self.config_entry, 
                 data={**self.config_entry.data, CONF_TIMEOUT: user_input[CONF_TIMEOUT]}
             )
             return self.async_create_entry(title="", data={})
 
-        # Берем таймаут из встроенного self.config_entry
+        # Берем таймаут из нативного self.config_entry
         current_timeout = self.config_entry.data.get(CONF_TIMEOUT, DEFAULT_TIMEOUT)
 
         # Показываем форму редактирования в UI
@@ -76,4 +74,5 @@ class JarvisVoiceOptionsFlowHandler(config_entries.OptionsFlow):
                 )
             })
         )
+
 
